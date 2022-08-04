4fe8e1a5a7d9bffd642cd57727546a4e6bab1840
iswarya <iswarya.ch99@gmail.com>
2022-08-04 23:50:25 +0530

    Solved Challenge #3

--- src/challenge3/index.ts
+++ src/challenge3/index.ts
@@ -1,21 +1,26 @@
-import { fromEvent, map, merge, tap } from 'rxjs';
+import { Observable, fromEvent, map, tap, merge } from 'rxjs';
 import '../header';
 
-const button1 = <HTMLButtonElement>document.querySelector('#button1')!;
-const button2 = <HTMLButtonElement>document.querySelector('#button2')!;
-const label = <HTMLLabelElement>document.querySelector('#label')!;
+const button1 = <HTMLButtonElement>document.getElementById('button1')!;
+const button2 = <HTMLButtonElement>document.getElementById('button2');
+const label = <HTMLElement>document.querySelector('label');
 
-const text1$ = fromEvent<Event>(button1, 'click').pipe(
-  map((event: Event) => (<HTMLButtonElement>event.target).textContent)
-);
-const text2$ = fromEvent<Event>(button2, 'click').pipe(
-  map((event: Event) => (<HTMLButtonElement>event.target).textContent)
+const buttonOneClick$: Observable<any> = fromEvent<Event>(button1, 'click').pipe(
+  map((event: Event) => <HTMLButtonElement>event.target)
 );
 
-merge(text1$, text2$)
+const buttonTwoClick$: Observable<any> = fromEvent<Event>(button2, 'click').pipe(
+  map((event: Event) => <HTMLButtonElement>event.target)
+);
+
+merge(buttonOneClick$, buttonTwoClick$) //Input Observables
   .pipe(
-    tap((value) => {
-      label.textContent = value;
+    // Output Observable
+    tap((target: HTMLButtonElement) => {
+      label.textContent = target.textContent;
     })
   )
   .subscribe();
+
+//merge() operator => join operator that is used to turn multiple observables into a single observable.
+// It creates an output Observable, which concurrently emits all values from every given input Observables.
